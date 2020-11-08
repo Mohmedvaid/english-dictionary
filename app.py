26554c6eeb05166420883b34feb206b0ce9c696f
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-11-08 17:26:09 -0600

    added suggestions

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 
 def translate(word):
-    return data["word"]
+    word.lower()
+    if word in data:
+        return data["word"]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return f"Did you mean '{get_close_matches(word, data.keys())[0]}'?"
+    else:
+        return "This word does not exists in the dictionary."
 
 word = input("Enter a word: ")
 print(translate(word))
